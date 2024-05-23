ded6dff5d8306723c4c44169339e123d89d5ba60
Santhoshi Pavani A <santhoshipavani-all@pluralsight.com>
2024-05-23 20:45:12 +0530

    Added enter button which on clicked displays a message

--- scripts/index.js
+++ scripts/index.js
@@ -3,35 +3,76 @@
 window.onload = load_teams;
 
 let teams = [
-    { teamcode:"DAL", 
-      name:"Dallas Cowboys", 
-      locatedIn:"Arlington, TX"
-    },
-    { teamcode:"DEN", 
-      name:"Denver Broncos", 
-      locatedIn:"Denver, CO"
-    },
-    { teamcode:"HOU", 
-      name:"Houston Texans", 
-      locatedIn:"Houston, TX"
-    },
-    { teamcode:"KAN", 
-      name:"Kansas City Chiefs", 
-      locatedIn:"Kansas City, MO"
-    }
-  ];
-  
-
-  function load_teams() {
-    const teamList = document.getElementById("teamlist");
-    for(let i=0; i<teams.length; i++) {
-
-        //Create the option element and set the 
-        //text and value
-        let theOption = new Option(teams[i].name, teams[i].teamcode);
-        teamList.appendChild(theOption);
-
-
-    }
+  {
+    teamcode: "DAL",
+    name: "Dallas Cowboys",
+    locatedIn: "Arlington, TX"
+  },
+  {
+    teamcode: "DEN",
+    name: "Denver Broncos",
+    locatedIn: "Denver, CO"
+  },
+  {
+    teamcode: "HOU",
+    name: "Houston Texans",
+    locatedIn: "Houston, TX"
+  },
+  {
+    teamcode: "KAN",
+    name: "Kansas City Chiefs",
+    locatedIn: "Kansas City, MO"
   }
-  
+];
+
+
+function load_teams() {
+  const teamList = document.getElementById("teamlist");
+  for (let i = 0; i < teams.length; i++) {
+
+    //Create the option element and set the 
+    //text and value
+    let theOption = new Option(teams[i].name, teams[i].teamcode);
+    teamList.appendChild(theOption);
+
+    const enterbutton = document.getElementById("enterbutton");
+    enterbutton.onclick = onEnterButtonClicked;
+  }
+}
+
+function ftbyname(arrayele) {
+  const teamList = document.getElementById("teamlist");
+  let selected_team = teamlist.value;
+
+  if (arrayele.teamcode == selected_team) {
+    console.log("Here");
+    return true;
+  }
+  else
+    return false;
+
+}
+
+
+function onEnterButtonClicked() {
+
+  let msgpara_ele = document.getElementById("msgpara");
+  let div1 = document.getElementById("div1");
+
+  if (msgpara_ele)
+    div1.removeChild(msgpara_ele);
+
+  const teamList = document.getElementById("teamlist");
+  let selected_team = teamlist.value;
+
+  let teamsarrayele = teams.find(ftbyname);
+
+  let pmsg = `You selected the ${teamsarrayele.name}  (${teamsarrayele.teamcode}) who play in ${teamsarrayele.locatedIn}`;
+  let p_ele = document.createElement("p");
+  p_ele.innerHTML = pmsg;
+  p_ele.id = "msgpara";
+  p_ele.style.fontWeight = "bold";
+
+  div1.appendChild(p_ele);
+
+}
